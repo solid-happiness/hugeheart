5e558eab9ea2f785782d24620f9703c4c88e9fd4
itumanov <van.tum@mail.ru>
2019-03-30 20:42:16 +0300

    Добавил view для аутентификации

--- server/main/views.py
+++ server/main/views.py
@@ -1,4 +1,7 @@
+import json
+
 from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
 
 from .models import Partner, UserProfile
 from events import models
@@ -16,3 +19,16 @@ def main(request):
         'partners': [partner.to_dict() for partner in partners],
         'volunteers': volunteers
     })
+
+
+def login_view(request):
+    params = json.loads(request.body.decode('utf8'))
+    username = params.get('username')
+    password = params.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'auth': 'true', **user.userprofile.to_dict()})
+
+    else:
+        return JsonResponse({'auth': 'false'})
